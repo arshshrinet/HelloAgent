9337a8ede1222d1c72fc02eed9cca9e1517936f7
arshitshrinet <singhsrinetarshit@gmail.com>
2026-09-16 08:41:19 +0530

    Add BasicAgent project with Azure OpenAI integration
    Added BasicAgent .NET 10.0 project with Azure.AI.OpenAI, Azure.Identity, and Microsoft.Agents.AI.OpenAI dependencies. Implemented Program.cs to create an AI agent, authenticate via Azure CLI, and demonstrate chat responses. Updated HelloAgent.slnx to include BasicAgent.

---
+++ BasicAgent/Program.cs
@@ -0,0 +1,37 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI.Chat;
+using System.ClientModel;
+
+
+//Define foundry Variable
+var endpoint = Environment.GetEnvironmentVariable("Azure_OpenAI_EndPoint") ?? throw new InvalidOperationException("Endpoint is not set.");
+var model = Environment.GetEnvironmentVariable("Azure_OpenAI_DeploymentName") ?? "gpt-5-mini";
+
+IChatClient chatClient = new AzureOpenAIClient(
+     new Uri(endpoint),
+    new AzureCliCredential()) //  this from cli
+    .GetChatClient(model)
+    .AsIChatClient();
+
+AIAgent agent = chatClient.AsAIAgent(
+    name: "NetworkSupport",
+    instructions: "You are tier 1 support agent. Your answer must be concise, professional and limited strict."
+    );
+Console.WriteLine($"Agent '{agent.Name}' is online!");
+
+string userIssue = "I am getting DNS resolution error when connecting to corporate vpn from coffee shop";
+
+//non streaming response 
+AgentResponse agentResponse = await agent.RunAsync(userIssue);
+Console.WriteLine(agentResponse.Text); //The entire response is availabe at once
+
+//streaming response
+await foreach (AgentResponseUpdate update in agent.RunStreamingAsync(userIssue))
+{
+    Console.Write(update.Text);
+}
+
+    
